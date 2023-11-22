1815ab256c45420d6ab5a4b3aa9a32c27ad4686b
Peyman <peiman_3009@yahoo.com>
2023-11-22 11:49:39 +0330

    remove unused generators

--- api/generate.ps1
+++
@@ -1,1 +0,0 @@
-protoc --dart_out=grpc:generated -Iproto common.proto -Iproto auth.proto -Iproto camera.proto -Iproto op.proto -Iproto report.proto -Iproto setting.proto -Iproto stream.proto -Iproto version.proto -Iproto user.proto -Iproto image.proto -Iproto subject.proto
